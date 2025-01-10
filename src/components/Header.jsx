7b3c5ddd6e9b328e0ce14a1c6ece45eca9755c5e
EnCo-de <142403662+EnCo-de@users.noreply.github.com>
2025-01-10 18:11:59 +0400

    create Header component,CSS styles

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import globe from '/images/globe.png'
+
+export default function Header() {
+    return <header>
+        <img src="{globe}" alt="globe">
+        <h1>Travel journey</h1>
+        <h2>React Vite project</h2>
+    </header>;
+}
