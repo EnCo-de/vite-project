386378cd339846adce7b74b5f191406415f9d2ef
EnCo-de <142403662+EnCo-de@users.noreply.github.com>
2025-01-10 19:16:03 +0400

    fix img tag

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import globe from '/images/globe.png'
 
 export default function Header() {
     return <header>
-        <img src="{globe}" alt="globe">
+        <img src="{globe}" alt="globe" />
         <h1>Travel journey</h1>
         <h2>React Vite project</h2>
     </header>;
