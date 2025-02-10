48be2534fa793604ee46cc8f6576e1ad641108cf
EnCo-de <142403662+EnCo-de@users.noreply.github.com>
2025-02-10 22:22:17 +0400

    hotfix

--- useState/src/Ingredients.jsx
+++ useState/src/Ingredients.jsx
@@ -37,7 +37,7 @@ export default function Ingredients() {
                 <h2>Ingredients on hand</h2>
                 <ul className="ingredients-list">{ingredientsList}</ul>
 
-                {props.ingredients.length >= 3 && <div className="get-recipe-container">
+                {ingredients.length >= 3 && <div className="get-recipe-container">
                     <div>
                         <h3>Ready for a recipe?</h3>
                         <p>Generate a recipe from your list of ingredients.</p>
